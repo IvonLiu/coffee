135b4b00cf78e570f734e68edc09bc1653cc06ca
Ivon Liu <me@ivonliu.com>
2017-04-02 19:11:29 -0700

    Sign up page UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
+
   firebase.initializeApp(config);
+
+  $('#form').submit(function(event) {
+
+  	event.preventDefault();
+
+  	name = $('#name-input').val();
+  	email = $('#email-input').val();
+  	existing = $('input[name="existing-input"]:checked').val();
+
+  	if (!(name && email && existing)) {
+  		alert('You must fill out all fields!');
+  		return false;
+  	}
+
+  	addNewUser(name, email, existing);
+
+  	$('#submit-btn').blur();
+  	document.getElementById('form').reset();
+
+  });
+
 });
+
+function addNewUser(name, email, existing) {
+
+}
